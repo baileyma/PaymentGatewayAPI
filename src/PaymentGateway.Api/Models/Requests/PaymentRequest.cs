cd886fc37d90045ba282c7e3d9901056a26bf77a
Matt Bailey <baileyma@tcd.ie>
2026-06-09 15:05:36 +0100

    add custom validation messages

--- src/PaymentGateway.Api/Models/Requests/PaymentRequest.cs
+++ src/PaymentGateway.Api/Models/Requests/PaymentRequest.cs
@@ -16,25 +16,30 @@ public class PaymentRequestValidator : AbstractValidator<PaymentRequest>
     private static readonly string[] SupportedISOCurrencyCodes = ["GBP", "EUR", "USD"];
     public PaymentRequestValidator()
     {
-        RuleFor(x => x.CardNumber).NotEmpty().Length(14, 19).Matches(@"^\d+$");
+        RuleFor(x => x.CardNumber).
+            NotEmpty().
+            Length(14, 19).
+            Matches(@"^\d+$").WithMessage("CardNumber must only contain numeric characters");
 
         RuleFor(x => x.Expiry).NotNull();
         RuleFor(x => x.Expiry.Month).InclusiveBetween(1, 12).When(x => x.Expiry != null);
-
-        RuleFor(x => x.Expiry).Must(e => new DateTime(e.Year, e.Month, 1) >= new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1))
-        .When(x => x.Expiry != null && x.Expiry.Month >= 1 && x.Expiry.Month <= 12);
+        RuleFor(x => x.Expiry).
+            Must(e => new DateTime(e.Year, e.Month, 1) >= new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1)).WithMessage("Expiry date cannot be in the past").
+            When(x => x.Expiry != null && x.Expiry.Month >= 1 && x.Expiry.Month <= 12);
 
         RuleFor(x => x.Money).NotNull();
         When(x => x.Money != null, () =>
         {
-            RuleFor(x => x.Money.Amount).NotEmpty();
-            RuleFor(x => x.Money.Currency)
-            .NotEmpty()
-            .Length(3)
-            .Must(c => SupportedISOCurrencyCodes.Contains(c))
-            .WithMessage("Currency must be one of: " + string.Join(", ", SupportedISOCurrencyCodes));
+            RuleFor(x => x.Money.Amount).NotEmpty().GreaterThan(0);
+            RuleFor(x => x.Money.Currency).
+                NotEmpty().
+                Length(3).
+                Must(c => SupportedISOCurrencyCodes.Contains(c)).WithMessage("Currency must be one of: " + string.Join(", ", SupportedISOCurrencyCodes));
         });
         
-        RuleFor(x => x.Cvv).NotEmpty().Length(3, 4).Matches(@"^\d+$");
+        RuleFor(x => x.Cvv).
+            NotEmpty().
+            Length(3, 4).
+            Matches(@"^\d+$").WithMessage("CardNumber must only contain numeric characters");
     }
 }
