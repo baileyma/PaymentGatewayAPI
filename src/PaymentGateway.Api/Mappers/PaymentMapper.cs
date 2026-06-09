fbdefb8480cd4f1642483505d6d8382e5073bbf1
Matt Bailey <baileyma@tcd.ie>
2026-06-09 16:42:23 +0100

    add response mapper

--- src/PaymentGateway.Api/Mappers/PaymentMapper.cs
+++ src/PaymentGateway.Api/Mappers/PaymentMapper.cs
@@ -1,5 +1,4 @@
-﻿using PaymentGateway.Api.Models.Common;
-using PaymentGateway.Api.Models.Enums;
+﻿using PaymentGateway.Api.Models.Enums;
 using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Models.Responses;
 
@@ -7,13 +6,21 @@ namespace PaymentGateway.Api.Mappers;
 
 public static class PaymentMapper
 {
-    public static BankRequest Map(PaymentRequest paymentRequest) =>
-        new BankRequest()
-        {
-            CardNumber = paymentRequest.CardNumber.ToString(),
-            ExpiryDate = paymentRequest.Expiry.ToString(),
-            Currency = paymentRequest.Money.Currency,
-            Amount = paymentRequest.Money.Amount,
-            CVV = paymentRequest.Cvv
-        };
+    public static BankRequest MapFromPaymentRequest(PaymentRequest paymentRequest) => new ()
+    {
+        CardNumber = paymentRequest.CardNumber.ToString(),
+        ExpiryDate = paymentRequest.Expiry.ToString(),
+        Currency = paymentRequest.Money.Currency,
+        Amount = paymentRequest.Money.Amount,
+        CVV = paymentRequest.Cvv
+    };
+
+    public static PaymentResponse MapToPaymentReponse(BankResponse bankClientResponse, PaymentRequest paymentRequest) => new()
+    {
+        Id = Guid.NewGuid(),
+        CardNumberLastFour = paymentRequest.CardNumber[^4..],
+        Expiry = paymentRequest.Expiry,
+        Money = paymentRequest.Money,
+        Status = bankClientResponse.Authorized ? PaymentStatus.Authorized : PaymentStatus.Declined
+    };
 }
